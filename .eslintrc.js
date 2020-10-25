01dc3cc573d3fa1db9e946ca4e0101eb6cc17fa4
William Sousa <will_ssa@outlook.com>
2020-10-25 19:26:36 -0300

    Initial function version app

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  extends: [
+    'react-app',
+    'standard',
+    'standard-jsx',
+    'standard-with-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'import',
+    'jsx-a11y'
+  ],
+  rules: {
+    "import/no-anonymous-default-export": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.tsx']
+      }
+    ]
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      typescript: {}
+    }
+  }
+}
